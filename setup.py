fd03fcddb71e3697bb0799dc68e72a61f3413c92
Chris Zieba <chris@lemonstand.com>
2014-04-30 17:10:07 -0700

    Prepare distribution

--- setup.py
+++ setup.py
@@ -2,16 +2,21 @@ from setuptools import setup
 
 setup(
     name="LemonSync",
-    version="0.1.0",
+    version="0.1.4",
     author='LemonStand',
     author_email='chris@lemonstand.com',
-    packages=['LemonSync', 'LemonSync.tests'],
-    scripts=['bin/LemonSync'],
+    download_url='https://github.com/lemonstand/LemonSync',
+    packages=['LemonSync'],
     license='LICENSE',
     description='LemonSync will listen for changes in the folder you configure, and automatically push updates to your store theme.',
     install_requires=[
         "watchdog >= 0.7.1",
         "requests >= 2.2.1",
         "boto >= 2.27.0"
-    ]
+    ],
+	entry_points={
+        'console_scripts': [
+            'lemonsync = LemonSync.LemonSync:main',
+        ]
+    }
 )
