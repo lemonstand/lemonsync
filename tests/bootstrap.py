6b1e414850eaef3ed58c1a26996f9e7e05b3745a
Chris Zieba <zieba.chris@gmail.com>
2014-05-11 22:26:01 -0700

    Add some more unit tests

--- tests/bootstrap.py
+++ tests/bootstrap.py
@@ -1,15 +1,18 @@
 #!/usr/bin/env python
 
+
+
 import unittest
 import sys
-import LemonSync
-from unit.utils import TestUtils
+from unit.test_utils import TestUtils
+from unit.test_upload import TestUpload
+
 
 def main ():
-    # Run the test suites
-    utils= unittest.TestLoader().loadTestsFromTestCase(TestUtils)
-    unittest.TestSuite([utils])
-    unittest.main(verbosity=2)
-    
+	# Run the test suites
+	utils= unittest.TestLoader().loadTestsFromTestCase(TestUtils)
+	unittest.TestSuite([utils])
+	unittest.main(verbosity=2)
+	
 if __name__ == '__main__':
-    main()
+	main()
--- tests/unit/config.py
+++
@@ -1,9 +0,0 @@
-base = 'https://lemonstand.com/'
-login = {
-	'email':'zieba.chris@gmail.com'
-}
-
-store = {
-	'front': 'https://gustavofring.lemonstand.com/',
-	'admin': 'https://gustavofring.lemonstand.com/backend/login/'
-}
