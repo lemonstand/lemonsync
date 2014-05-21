f1d2dd0d44d7635d57be615c4b0575f2845b4412
Chris Zieba <chris@lemonstand.com>
2014-05-21 13:14:37 -0700

    Fix exception syntax

--- LemonSync/Utils.py
+++ LemonSync/Utils.py
@@ -231,7 +231,7 @@ class Utils ():
 					os.unlink(file_path)
 				elif os.path.isdir(file_path):
 					shutil.rmtree(file_path)
-			except Exception, e:
+			except Exception as e:
 				print(e + Style.RESET_ALL)
 		return
 
